dacc85d0449282dee25177f7a890146310947ac2
juvieespinosa <jhuv89@gmail.com>
2022-10-27 13:08:26 -0700

    updated main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///planner.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
